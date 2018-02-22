e4b8756e40333ec4fa7b745374bd3f6a1acf0d1c
MrCocoaCat <liyubo_liyubo@163.com>
2018-02-23 00:13:22 +0800

    add src from paopao

---
+++ cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ORB_SLAM2.dir/src/System.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/Tracking.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/LocalMapping.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/LoopClosing.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/ORBextractor.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/ORBmatcher.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/FrameDrawer.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/Converter.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/MapPoint.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/KeyFrame.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/Map.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/MapDrawer.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/Optimizer.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/PnPsolver.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/Frame.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/KeyFrameDatabase.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/Sim3Solver.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/Initializer.cpp.o"
+  "CMakeFiles/ORB_SLAM2.dir/src/Viewer.cpp.o"
+  "../lib/libORB_SLAM2.pdb"
+  "../lib/libORB_SLAM2.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ORB_SLAM2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
