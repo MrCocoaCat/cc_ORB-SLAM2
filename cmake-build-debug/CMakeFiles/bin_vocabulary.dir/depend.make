6765e91e228eb7b0e872491d43899c35c6910111
liyubo <liyubo_liyubo@163.com>
2018-03-04 15:21:23 +0800

    add build.sh

--- cmake-build-debug/CMakeFiles/bin_vocabulary.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for bin_vocabulary.
-# This may be replaced when dependencies are built.
---
+++ unbuild.sh
@@ -0,0 +1,17 @@
+echo "delete building Thirdparty/DBoW2 ..."
+
+cd Thirdparty/DBoW2
+rm -rf build
+
+cd ../g2o
+
+echo "delete building Thirdparty/g2o ..."
+
+rm -rf build
+
+cd ../../
+
+
+echo "delete building ORB_SLAM2 ..."
+
+rm -rf build
